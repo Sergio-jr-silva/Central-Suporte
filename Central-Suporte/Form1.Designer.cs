f7be46db7343a32314ef412a5ff766e3df08de33
Sergio_jrsilva <juniosergio034@gmail.com>
2025-05-01 12:23:33 -0300

    central-suporte

---
+++ Central-Suporte/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Central_Suporte
+{
+    partial class CentralSuporte
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            iconButton6 = new FontAwesome.Sharp.IconButton();
+            iconButton5 = new FontAwesome.Sharp.IconButton();
+            iconButton4 = new FontAwesome.Sharp.IconButton();
+            iconButton3 = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            Id = new DataGridViewTextBoxColumn();
+            Titulo = new DataGridViewTextBoxColumn();
+            Prioridade = new DataGridViewTextBoxColumn();
+            Data = new DataGridViewTextBoxColumn();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonFace;
+            panel1.Controls.Add(iconButton6);
+            panel1.Controls.Add(iconButton5);
+            panel1.Controls.Add(iconButton4);
+            panel1.Controls.Add(iconButton3);
+            panel1.Controls.Add(iconButton2);
+            panel1.Controls.Add(iconButton1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(265, 495);
+            panel1.TabIndex = 0;
+            // 
+            // iconButton6
+            // 
+            iconButton6.BackColor = Color.WhiteSmoke;
+            iconButton6.Dock = DockStyle.Top;
+            iconButton6.FlatAppearance.BorderSize = 0;
+            iconButton6.FlatStyle = FlatStyle.Flat;
+            iconButton6.Font = new Font("Arial Narrow", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton6.IconChar = FontAwesome.Sharp.IconChar.ChartLine;
+            iconButton6.IconColor = Color.DarkGreen;
+            iconButton6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton6.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton6.Location = new Point(0, 431);
+            iconButton6.Name = "iconButton6";
+            iconButton6.Size = new Size(265, 64);
+            iconButton6.TabIndex = 6;
+            iconButton6.Text = "Monitorar Desempenho";
+            iconButton6.UseVisualStyleBackColor = false;
+            // 
+            // iconButton5
+            // 
+            iconButton5.BackColor = Color.WhiteSmoke;
+            iconButton5.Dock = DockStyle.Top;
+            iconButton5.FlatAppearance.BorderSize = 0;
+            iconButton5.FlatStyle = FlatStyle.Flat;
+            iconButton5.Font = new Font("Arial Narrow", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton5.IconChar = FontAwesome.Sharp.IconChar.Database;
+            iconButton5.IconColor = Color.DarkGreen;
+            iconButton5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton5.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton5.Location = new Point(0, 375);
+            iconButton5.Name = "iconButton5";
+            iconButton5.Size = new Size(265, 56);
+            iconButton5.TabIndex = 5;
+            iconButton5.Text = "Status dos  PCs e Servidores";
+            iconButton5.TextAlign = ContentAlignment.MiddleRight;
+            iconButton5.UseVisualStyleBackColor = false;
+            // 
+            // iconButton4
+            // 
+            iconButton4.BackColor = Color.WhiteSmoke;
+            iconButton4.Dock = DockStyle.Top;
+            iconButton4.FlatAppearance.BorderSize = 0;
+            iconButton4.FlatStyle = FlatStyle.Flat;
+            iconButton4.Font = new Font("Arial Narrow", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton4.IconChar = FontAwesome.Sharp.IconChar.NetworkWired;
+            iconButton4.IconColor = Color.DarkGreen;
+            iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton4.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton4.Location = new Point(0, 317);
+            iconButton4.Name = "iconButton4";
+            iconButton4.Size = new Size(265, 58);
+            iconButton4.TabIndex = 4;
+            iconButton4.Text = "Verificar Rede";
+            iconButton4.UseVisualStyleBackColor = false;
+            // 
+            // iconButton3
+            // 
+            iconButton3.BackColor = Color.WhiteSmoke;
+            iconButton3.Dock = DockStyle.Top;
+            iconButton3.FlatAppearance.BorderSize = 0;
+            iconButton3.FlatStyle = FlatStyle.Flat;
+            iconButton3.Font = new Font("Arial Narrow", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton3.IconChar = FontAwesome.Sharp.IconChar.Cogs;
+            iconButton3.IconColor = Color.DarkGreen;
+            iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton3.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton3.Location = new Point(0, 257);
+            iconButton3.Name = "iconButton3";
+            iconButton3.Size = new Size(265, 60);
+            iconButton3.TabIndex = 3;
+            iconButton3.Text = "Limpeza Automática";
+            iconButton3.UseVisualStyleBackColor = false;
+            // 
+            // iconButton2
+            // 
+            iconButton2.BackColor = Color.WhiteSmoke;
+            iconButton2.Dock = DockStyle.Top;
+            iconButton2.FlatAppearance.BorderSize = 0;
+            iconButton2.FlatStyle = FlatStyle.Flat;
+            iconButton2.Font = new Font("Arial Narrow", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.Broom;
+            iconButton2.IconColor = Color.DarkGreen;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton2.Location = new Point(0, 194);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Size = new Size(265, 63);
+            iconButton2.TabIndex = 2;
+            iconButton2.Text = "Limpeza Manual";
+            iconButton2.UseVisualStyleBackColor = false;
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = Color.WhiteSmoke;
+            iconButton1.Dock = DockStyle.Top;
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.Font = new Font("Arial Narrow", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.EnvelopeOpen;
+            iconButton1.IconColor = Color.DarkGreen;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton1.Location = new Point(0, 130);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(265, 64);
+            iconButton1.TabIndex = 1;
+            iconButton1.Text = "Chamados em Aberto";
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(265, 130);
+            panel2.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.Location = new Point(264, 2);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(825, 125);
+            panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(261, 5);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(250, 125);
+            panel3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 16.2F, FontStyle.Bold);
+            label2.Location = new Point(58, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(135, 33);
+            label2.TabIndex = 2;
+            label2.Text = "de Suporte";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 16.2F, FontStyle.Bold);
+            label1.Location = new Point(38, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 33);
+            label1.TabIndex = 1;
+            label1.Text = "Gerenciamento";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(313, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(253, 33);
+            label3.TabIndex = 1;
+            label3.Text = "Chamados em Aberto";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Id, Titulo, Prioridade, Data });
+            dataGridView1.Location = new Point(298, 81);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(554, 350);
+            dataGridView1.TabIndex = 2;
+            // 
+            // Id
+            // 
+            Id.HeaderText = "Id";
+            Id.MinimumWidth = 6;
+            Id.Name = "Id";
+            Id.Width = 125;
+            // 
+            // Titulo
+            // 
+            Titulo.HeaderText = "Titulo";
+            Titulo.MinimumWidth = 6;
+            Titulo.Name = "Titulo";
+            Titulo.Width = 125;
+            // 
+            // Prioridade
+            // 
+            Prioridade.HeaderText = "Prioridade";
+            Prioridade.MinimumWidth = 6;
+            Prioridade.Name = "Prioridade";
+            Prioridade.Width = 125;
+            // 
+            // Data
+            // 
+            Data.HeaderText = "Data da Abertura";
+            Data.MinimumWidth = 6;
+            Data.Name = "Data";
+            Data.Width = 125;
+            // 
+            // CentralSuporte
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(1090, 495);
+            Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = Color.DarkSlateGray;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "CentralSuporte";
+            Text = "Central Suporte";
+            Load += CentralSuporte_Load;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Panel panel2;
+        private Label label1;
+        private Label label2;
+        private FontAwesome.Sharp.IconButton iconButton5;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private FontAwesome.Sharp.IconButton iconButton6;
+        private Panel panel4;
+        private Panel panel3;
+        private Label label3;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn Titulo;
+        private DataGridViewTextBoxColumn Prioridade;
+        private DataGridViewTextBoxColumn Data;
+    }
+}
---
+++ Central-Suporte/Form1.cs
@@ -0,0 +1,36 @@
+namespace Central_Suporte
+{
+    public partial class CentralSuporte : Form
+    {
+        public CentralSuporte()
+        {
+            InitializeComponent();
+        }
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void CentralSuporte_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
